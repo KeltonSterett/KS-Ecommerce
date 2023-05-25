88cd7c1d7dc68a0fa749f3abcb0325dce4e66700
KeltonSterett <keltonsterett24@gmail.com>
2023-05-25 11:36:09 -0600

    added assocciation methods

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,23 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category (as a category can have multiple products but a product can only belong to one category) 
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag) as a product can have multiple tags and a tag can have many products
+Product.belongsToMany(Tag, { 
+  through: ProductTag,
+  foreignKey: 'product_id'
+ });
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag) as a tag can have many products and a product can have many tags
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
